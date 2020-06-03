201d7fdd986fec03ba17bcf067236235ca9f2ae3
Sean AP <66278738+2linebot@users.noreply.github.com>
2020-06-03 16:36:52 -0400

    Update 1line.py

--- 1line.py
+++ 1line.py
@@ -16,14 +16,14 @@ api=tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
 user=api.me()
 
 search='"Line 1" from:TTCnotices'
-nrTweets=2880
+nrTweets=5
 
 for tweet in tweepy.Cursor(api.search, search).items(nrTweets):
     try:
         print('Alert RTd')
         print(tweet.text)
         tweet.retweet()
-        time.sleep(30)
+        time.sleep(5)
     except tweepy.TweepError as e:
         print(e.reason)
     except StopIteration:
